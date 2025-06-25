a18e68d4d0c549bf69bd863fdc3b68551e8d3194
Tamar Karlinski <Tamar.Karlinski@qnomy.com>
2025-06-25 14:38:19 +0300

    First commit - upload project

---
+++ Server/Server/BLL/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿using Server.Models;
+using Server.DAL;
+
+namespace Server.BLL
+{
+    public class CategoryService
+    {
+        private readonly CategoryRepository _repo;
+        public CategoryService(CategoryRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<List<CategoryDto>> GetCategoryNamesAsync()
+        {
+            var categories = await _repo.GetAllAsync();
+            return categories
+                .Select(c => new CategoryDto { Id = c.Id, Name = c.Name })
+                .ToList();
+        }
+    }
+}
---
+++ Server/Server/DAL/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Server.Models;
+using Server.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Server.DAL
+{
+    public class CategoryRepository
+    {
+        private readonly AppDbContext _context;
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+    }
+}
